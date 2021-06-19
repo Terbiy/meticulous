b5e84b62b00ad3c32846ea6ec1b2627de48e053e
German Tebiev <terbiyarchitect@gmail.com>
2021-06-19 23:05:18 +0300

    Task #1: Implement the Card component emphasized styles

--- src/Card.tsx
+++ src/Card.tsx
@@ -3,13 +3,18 @@ import React from "react";
 type CardProps = {
   title?: string;
   text?: string;
+  emphasized?: boolean;
 };
 
-export function Card({ title, text }: CardProps) {
+export function Card({ title = "", text = "", emphasized = false }: CardProps) {
   if (!(title || text)) return null;
 
+  const classes = ["Card", emphasized && "Card--emphasized"]
+    .filter(Boolean)
+    .join(" ");
+
   return (
-    <section className="Card">
+    <section className={classes}>
       <h2 className="Card__Title">{title}</h2>
       <p className="Card__Text">{text}</p>
     </section>
